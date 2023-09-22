b063df38be625cdff6cf981a8553dd633794cf15
carlosftm <carlosftm@gmail.com>
2023-09-22 18:23:55 +0200

    Initial Commit

---
+++ 01_SingleTask/main.c
@@ -0,0 +1,36 @@
+// Copyright (c) 2023 CarlosFTM
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+
+/* Task: Toggle LED */
+void taskToggleLed( void *pvParameters )
+{
+    int ledState = 1;
+
+    while( 1 )
+    {
+        ledState = ledState ^ 1;
+        gpio_put(PICO_DEFAULT_LED_PIN, ledState );
+        printf( "Toggle Led [%d]\n\r", ledState );
+        vTaskDelay( 500 );
+    }
+}
+
+/* ***********************************************
+ * Main function
+ * ********************************************* */
+void main( void )
+{
+    stdio_init_all( );
+    gpio_init( PICO_DEFAULT_LED_PIN );
+    gpio_set_dir( PICO_DEFAULT_LED_PIN, GPIO_OUT );
+
+    printf( "Hola");
+    xTaskCreate( taskToggleLed, "Toggle_LED", 256, NULL, 1, NULL );
+    vTaskStartScheduler( );
+    while( 1 );
+}
